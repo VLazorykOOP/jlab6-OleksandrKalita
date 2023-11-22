0de19111b60148fc36cc76c233cfb5ce1e509137
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-11-22 14:44:50 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(" Java Lab #6 ");
-	// write your code here
+        String[] strings = { "Рядок 1", "Рядок 2", "Рядок 3", "Рядок 4", "Рядок 5" };
+
+        moveStrings(strings);
+    }
+
+    public static void moveStrings(String[] strings) {
+        Random random = new Random();
+        int frameWidth = 100;
+
+        int[] positions = new int[strings.length];
+
+        while (true) {
+            for (int i = 0; i < strings.length; i++) {
+                StringBuilder spaces = new StringBuilder();
+                for (int j = 0; j < positions[i]; j++) {
+                    spaces.append(" ");
+                }
+                System.out.println(spaces.toString() + strings[i]);
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < strings.length; i++) {
+                positions[i] = (positions[i] + random.nextInt(5)) % frameWidth; // Зміна позиції випадковим чином
+            }
+        }
     }
 }
---
+++ src/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+for (int i = 0; i < strings.length; i++) {
+            //     StringBuilder spaces = new StringBuilder();
+            //     for (int j = 0; j < frameWidth; j++) {
+            //         spaces.append(" ");
+            //     }
+            //     System.out.println(spaces.toString());
+            // }
